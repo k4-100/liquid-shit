cd583d52e4dfbb4c73d6e8a6542a24486f2bb22c
your@example.com <your@example.com>
2026-06-14 13:54:18 +0200

    working on pulling out data about block

--- src/main/java/com/example/cuboidcheck/commands/CuboidCheckRestoreCommand.java
+++ src/main/java/com/example/cuboidcheck/commands/CuboidCheckRestoreCommand.java
@@ -1,10 +1,19 @@
 
 package com.example.cuboidcheck.commands;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class CuboidCheckRestoreCommand {
 
@@ -14,9 +23,30 @@ public class CuboidCheckRestoreCommand {
         Commands.literal("cuboidrestore")
             .requires(source -> source.hasPermission(2)) // OP level 2
             .executes(context -> {
+              MinecraftServer server = context.getSource().getServer();
+              ServerLevel level = server.getLevel(Level.OVERWORLD);
+              // BlockPos pos1 = new BlockPos(-50, 0, -50);
+              // BlockPos pos2 = new BlockPos(50, 200, 50);
+              BlockPos pos1 = new BlockPos(-5, 0, -5);
+              BlockPos pos2 = new BlockPos(5, 5, 5);
 
+              Map<BlockPos, BlockState> blockMap = new HashMap<>();
+              for (BlockPos pos : BlockPos.betweenClosed(pos1, pos2)) {
+                blockMap.put(pos.immutable(), level.getBlockState(pos));
+
+              }
+              // String msg = "#####DATA######\n";
+              // for (BlockPos bp : blockMap.keySet()) {
+              // msg += bp.toString() + "\n";
+              // }
+              String msg = "#####DATA######\n"
+                  + blockMap.keySet().stream().map(k -> k.toString()).collect(Collectors.joining())
+                  + "\n#####DATA#####";
+
+              final String finalmsg = msg;
               context.getSource().sendSuccess(
-                  () -> Component.literal("RESTORING"),
+                  () -> Component.literal(finalmsg),
+                  // () -> Component.literal("RESTORING"),
                   false);
 
               return 1;
